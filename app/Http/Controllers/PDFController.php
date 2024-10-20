106f83ebde3d592b928777d21b399acb7245a1b6
AraceliPac <a23arapacmun@inspedralbes.cat>
2024-10-20 19:38:04 +0200

    add email with pdf

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\Mail;
 
 class PDFController extends Controller
 {
@@ -19,6 +20,14 @@ class PDFController extends Controller
             'users' => $users
         ];
         $pdf = PDF::loadView('myPDF', $data);
+        $pdfOutput = $pdf->output();
+        Mail::send('emails.contactanos', [], function ($message) use ($pdfOutput) {
+            $message->to('a23arapacmun@inspedralbes.cat')
+                ->subject('Datos de Usuarios PDF')
+                ->attachData($pdfOutput, 'documento.pdf', [
+                    'mime' => 'application/pdf',
+                ]);
+        });
 
         return $pdf->stream('documento.pdf');
     }
--- resources/views/emails/contactanos.blade.php
+++ resources/views/emails/contactanos.blade.php
@@ -2,4 +2,5 @@
     <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
     <h1>MI PRIMER CORREO</h1>
     <p>Este es el primer correo que envio por Laravel. Un saludo!</p>
+    <p>Adjunto PDF con los datos de los usuarios.</p>
 </div>
